b5738634ba75a104ee6e9d12394580d3484a58c2
POLIT3 <ryannduva@gmail.com>
2026-02-15 20:31:02 +0300

    Profile Picture Added & button functionality

--- public/logic.js
+++ public/logic.js
@@ -1,6 +1,14 @@
+
 function forwardWhatsApp() {
 	const phone = '+254717563161';
 	const message = encodeURIComponent('Hello Victor, I would like to request your services.');
+	const url = `https://wa.me/${phone.replace('+', '')}?text=${message}`;
+	window.open(url, '_blank');
+}
+
+function forwardWhatsAppService(service) {
+	const phone = '+254717563161';
+	const message = encodeURIComponent(`Hello Victor, I would like to request your service: ${service}`);
 	const url = `https://wa.me/${phone.replace('+', '')}?text=${message}`;
 	window.open(url, '_blank');
 }
